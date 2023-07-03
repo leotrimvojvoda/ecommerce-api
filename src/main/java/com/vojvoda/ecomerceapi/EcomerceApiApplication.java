234226ac8d51a2035851e44bd17c22cb3f0da362
leotrimvojvoda <leotrimvojvoda@gmail.com>
2023-07-03 12:44:06 +0200

    Initial Commit

---
+++ src/main/java/com/vojvoda/ecomerceapi/EcomerceApiApplication.java
@@ -0,0 +1,13 @@
+package com.vojvoda.ecomerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcomerceApiApplication.class, args);
+    }
+
+}
